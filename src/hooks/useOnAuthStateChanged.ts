3db9b4d65a3d40d86c60c0b601a04959d87d053d
romeusorionaet <romeusoares14569@gmail.com>
2023-09-17 20:16:43 -0300

    create hook for authState

--- src/hooks/useOnAuthStateChanged.ts
+++ src/hooks/useOnAuthStateChanged.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
 import { onAuthStateChanged } from '@firebase/auth'
 import { auth } from '@/services/firebaseConfig'
+import { useEffect, useState } from 'react'
 
 export function useOnAuthenticated() {
   const [userId, setUserId] = useState<string | null>('')
