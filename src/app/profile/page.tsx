2830555a35008c18ca470f0c30316948b7af4b11
romeusorionaet <romeusoares14569@gmail.com>
2023-09-18 12:13:15 -0300

    change name routes and clean Header

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,40 +1,65 @@
 'use client'
 
-import React, { useState } from 'react'
-import { motion, useAnimation } from 'framer-motion'
-import { FormProfile } from './components/FormProfile'
-import { FormPassword } from './components/FormPassword'
-import { Key } from 'phosphor-react'
+import { CardGoal, CardGoalProps } from '@/components/CardGoal'
+import { db } from '@/services/firebaseConfig'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+
+import * as Checkbox from '@radix-ui/react-checkbox'
+import { Check } from 'phosphor-react'
+import { useOnAuthenticated } from '@/hooks/useOnAuthStateChanged'
 
 export default function Profile() {
-  const [isPasswordFormHidden, setIsPasswordFormHidden] = useState(true)
-  const controls = useAnimation()
-  const rotationClass = isPasswordFormHidden
-    ? 'rotate-180 duration-700'
-    : 'duration-700'
+  const [cardGoal, setCardGoal] = useState<CardGoalProps[]>([])
+  const [checkboxState, setCheckboxState] = useState(false)
+  const { userId } = useOnAuthenticated()
 
-  const toggleFormVisibility = () => {
-    controls.start({ opacity: 0, scale: 1 })
-    setTimeout(() => {
-      setIsPasswordFormHidden((prev) => !prev)
-      controls.start({ opacity: 1, scale: 1 })
-    }, 300)
+  useEffect(() => {
+    const getGoal = async () => {
+      const querySnapshot = await getDocs(
+        query(collection(db, 'cardGoal'), where('userId', '==', userId)),
+      )
+
+      const goals: CardGoalProps[] = []
+
+      querySnapshot.forEach((doc) => {
+        const data = doc.data() as CardGoalProps
+        goals.push(data)
+      })
+
+      setCardGoal(goals)
+    }
+    getGoal()
+  }, [userId])
+
+  function handleCheckBoxState() {
+    checkboxState ? setCheckboxState(false) : setCheckboxState(true)
   }
 
   return (
-    <div className="fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] space-y-8 rounded-xl bg-white p-4 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-      <button className="absolute right-6 top-6" onClick={toggleFormVisibility}>
-        <Key className={`h-6 w-6 ${rotationClass}`} />
-      </button>
+    <div className="flex flex-wrap items-center justify-center gap-2">
+      {cardGoal &&
+        cardGoal.map((card) => {
+          return (
+            <div className="relative" key={card.cardId}>
+              <CardGoal
+                startDate={card.startDate}
+                finalDate={card.finalDate}
+                photoURL={card.photoURL}
+                goal={card.goal}
+              />
 
-      <motion.div
-        initial={{ opacity: 1, scale: 1, height: 'auto' }}
-        animate={controls}
-        transition={{ duration: 0.6 }}
-        style={{ overflow: 'hidden' }}
-      >
-        {isPasswordFormHidden ? <FormProfile /> : <FormPassword />}
-      </motion.div>
+              <Checkbox.Root
+                onClick={handleCheckBoxState}
+                className="shadow-blackA7 hover:bg-violet3 absolute right-2 top-2 flex h-5 w-5 appearance-none items-center justify-center rounded-md bg-white shadow-[0_2px_10px] outline-none focus:shadow-[0_0_0_2px_black]"
+              >
+                <Checkbox.Indicator>
+                  <Check />
+                </Checkbox.Indicator>
+              </Checkbox.Root>
+            </div>
+          )
+        })}
     </div>
   )
 }
